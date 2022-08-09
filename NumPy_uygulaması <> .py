e9435b4146ea7b5cbf39fc1c1ed28573c3fd0de9
SAMİ GÜNDOĞAN <102728972+WhoCodes1299@users.noreply.github.com>
2022-08-09 19:38:26 +0300

    Add files via upload
    DS AI ML

---
+++ NumPy_uygulaması <> .py
@@ -0,0 +1,18 @@
+import numpy as np
+
+dizi1 = np.array([7, 8, 2, 5])
+dizi2 = np.array([2, 5, 9, 8])
+
+print("Toplam :", dizi1 + dizi2)
+
+print("Fark :", dizi1 - dizi2)
+
+print("Çarpım :", dizi1 * dizi2)
+
+print("Bölme (tam) :", dizi1 // dizi2)
+
+print("Bölme (ondalık) :", dizi1 / dizi2)
+
+print("Bölümden kalan :", dizi1 % dizi2)
+
+print("Üst :", dizi1 ** dizi2)
